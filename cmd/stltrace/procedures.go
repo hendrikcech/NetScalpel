0524ad679f6d1f75a2bbf5ff9068b5b0f298d1c8
Hendrik Cech <hendrik.cech@gmail.com>
2025-07-26 06:23:26 +0200

    Add additional procedures

--- cmd/stltrace/procedures.go
+++ cmd/stltrace/procedures.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log/slog"
-	"math"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -143,7 +142,8 @@ func (e *Executor) ProgressiveDurationMultiRate(ts time.Time, resultPath string,
 	deadline := nextRi(ts).Add(-time.Second)
 
 	// durationsMs := []int{100, 300, 500, 700, 900, 1400, 2000}
-	durationsMs := []uint{100, 300, 500, 700}
+	// durationsMs := []uint{100, 300, 500, 700}
+	durationsMs := []uint{2000}
 	if _, ok := params["durations"]; ok {
 		var err error
 		if durationsMs, err = params.Uints("durations"); err != nil {
@@ -154,10 +154,14 @@ func (e *Executor) ProgressiveDurationMultiRate(ts time.Time, resultPath string,
 
 	var ratesMbps []uint
 	if direction == pkg.UL {
-		ratesMbps = []uint{70, 35, 10, 5, 1}
+		// ratesMbps = []uint{70, 35, 10, 5, 1}
+		ratesMbps = []uint{50, 35, 25, 20}
 	} else {
 		// ratesMbps = []uint{700, 350, 100, 5, 1}
-		ratesMbps = []uint{500, 250}
+		// ratesMbps = []uint{500, 250, 200}
+		// ratesMbps = []uint{175, 150, 125}
+		// ratesMbps = []uint{700, 600, 500}
+		ratesMbps = []uint{550, 600, 650}
 	}
 
 	for _, rateIdx := range rand.Perm(len(ratesMbps)) {
@@ -286,7 +290,7 @@ func (e *Executor) BurstRi(ts time.Time, resultPath string, params ParamMap) err
 	return nil
 }
 
-func (e *Executor) CoolDown(ts time.Time, resultPath string, params ParamMap) error {
+func (e *Executor) CoolDownDifferentFlow(ts time.Time, resultPath string, params ParamMap) error {
 	direction, err := params.Direction()
 	if err != nil {
 		return fmt.Errorf("Procedure requires valid 'direction' param: %v", err.Error())
@@ -294,6 +298,7 @@ func (e *Executor) CoolDown(ts time.Time, resultPath string, params ParamMap) er
 
 	start := ts.Add(1 * time.Second)
 	duration := time.Duration(800) * time.Millisecond
+	spacing := time.Duration(2000) * time.Millisecond
 	deadline := nextRi(ts).Add(-time.Second)
 
 	var pps uint
@@ -303,45 +308,34 @@ func (e *Executor) CoolDown(ts time.Time, resultPath string, params ParamMap) er
 		pps = 700 * 1e6 / 8 / 1400
 	}
 
-	e.RunClient(&pkg.SenderClient{
-		IP:        e.IP,
-		Out:       filepath.Join(resultPath, fmt.Sprintf("rate_%v_init.csv", direction.StringLower())),
-		Direction: direction,
-		StartAt:   start,
-		Sender: &pkg.RateSender{Params: []pkg.RateParams{pkg.RateParams{
-			Pps:         pps,
-			Interval:    time.Millisecond,
-			Duration:    duration,
-			PayloadSize: 1400,
-		}}},
-	})
-	start = start.Add(duration)
-
 	// coolDowns := []int{0, 10, 50, 100, 500, 1000, 4000}
 	// for _, idx := range rand.Perm(len(coolDowns)) {
-	var coolDowns []int
+	var coolDowns []int64
+outer:
 	for {
 		// coolDownMs := coolDowns[idx]
-		// coolDownMs := 130 + rand.Intn(3000)
+		coolDownMs := int64(rand.Intn(400)) / 10 * 10 // Limit to multiples of 10
+		coolDownDuration := time.Duration(coolDownMs) * time.Millisecond
 
-		x := rand.ExpFloat64() / 1.5
-		y := 2 / math.Pi * math.Atan(x)
-		minCoolDownMs := 200
-		maxCoolDownMs := min(deadline.Sub(start.Add(duration)).Milliseconds(), 3000-int64(minCoolDownMs))
-		coolDownMs := minCoolDownMs + int(y*float64(maxCoolDownMs))
+		for _, v := range coolDowns {
+			if coolDownMs == v {
+				// Don't test the same coolDown twice in one round to make it easier for analysis scripts
+				continue outer
+			}
+		}
 
 		// Add cool down before starting rate test
-		nextStart := start.Add(time.Duration(coolDownMs) * time.Millisecond)
-		if start.Add(duration).After(deadline) {
+		nextStart := start.Add(duration).Add(coolDownDuration).Add(duration)
+		if start.After(deadline) {
 			// Would take too long
 			break
 		}
-		start = nextStart
 		coolDowns = append(coolDowns, coolDownMs)
 
+		// Prime the link
 		e.RunClient(&pkg.SenderClient{
 			IP:        e.IP,
-			Out:       filepath.Join(resultPath, fmt.Sprintf("rate_%v_%04d.csv", direction.StringLower(), coolDownMs)),
+			Out:       filepath.Join(resultPath, fmt.Sprintf("rate_%v_%04d_a.csv", direction.StringLower(), coolDownMs)),
 			Direction: direction,
 			StartAt:   start,
 			Sender: &pkg.RateSender{Params: []pkg.RateParams{pkg.RateParams{
@@ -352,17 +346,34 @@ func (e *Executor) CoolDown(ts time.Time, resultPath string, params ParamMap) er
 			}}},
 		})
 
-		// Start next test after this one was run
+		start = start.Add(duration).Add(coolDownDuration)
+
+		// Test after a gap
+		e.RunClient(&pkg.SenderClient{
+			IP:        e.IP,
+			Out:       filepath.Join(resultPath, fmt.Sprintf("rate_%v_%04d_b.csv", direction.StringLower(), coolDownMs)),
+			Direction: direction,
+			StartAt:   start,
+			Sender: &pkg.RateSender{Params: []pkg.RateParams{pkg.RateParams{
+				Pps:         pps,
+				Interval:    time.Millisecond,
+				Duration:    duration,
+				PayloadSize: 1400,
+			}}},
+		})
+
 		start = start.Add(duration)
+
+		if start != nextStart {
+			panic("start != nextStart")
+		}
+
+		start = start.Add(spacing)
 	}
 
 	slog.Info(fmt.Sprintf("Scheduled cooldowns: %v", coolDowns))
 
 	e.tcpdump(resultPath, ts, start.Sub(ts)+time.Second)
-
-	if start.After(nextRi(ts).Add(time.Second)) {
-		return fmt.Errorf("Test takes longer than one RI")
-	}
 
 	return nil
 }
@@ -388,9 +399,17 @@ func (e *Executor) CoolDownSameFlow(ts time.Time, resultPath string, params Para
 	// coolDowns := []int{0, 5, 10, 25, 50, 100, 500, 1000}
 	// for _, idx := range rand.Perm(len(coolDowns)) {
 	var coolDowns []int64
+outer:
 	for {
 		// coolDownMs := coolDowns[idx]
-		coolDownMs := int64(rand.Intn(1000))
+		coolDownMs := int64(rand.Intn(600)) / 10 * 10 // Limit to multiples of 10
+
+		for _, v := range coolDowns {
+			if coolDownMs == v {
+				// Don't test the same coolDown twice in one round to make it easier for analysis scripts
+				continue outer
+			}
+		}
 
 		// Exponential distribution: test small values more
 		// x := rand.ExpFloat64() / 2      // increase rate / lambda
@@ -555,6 +574,103 @@ func (e *Executor) MultiFlow(ts time.Time, resultPath string, params ParamMap) e
 	return nil
 }
 
+func (e *Executor) MouseElephantFlows(ts time.Time, resultPath string, params ParamMap) error {
+	direction, err := params.Direction()
+	if err != nil {
+		return fmt.Errorf("Procedure requires valid 'direction' param: %v", err.Error())
+	}
+
+	start := ts.Add(1 * time.Second)
+	deadline := nextRi(ts).Add(-time.Second)
+	duration := time.Duration(800) * time.Millisecond
+	spacing := time.Duration(2000) * time.Millisecond
+
+	var elephMbps uint
+	var miceMbps []uint
+	if direction == pkg.UL {
+		elephMbps = 70
+		miceMbps = []uint{1, 5, 10, 15, 30, 70}
+	} else {
+		elephMbps = 700
+		miceMbps = []uint{1, 5, 15, 30, 700}
+	}
+
+	offsets := []time.Duration{
+		// -600 * time.Millisecond,
+		// -400 * time.Millisecond,
+		// -200 * time.Millisecond,
+		0 * time.Millisecond,
+		// 200 * time.Millisecond,
+		// 400 * time.Millisecond,
+		// 600 * time.Millisecond,
+	}
+
+outer:
+	// for i := 0; ; i++ {
+	// for _, idx := range rand.Perm(len(offsets)) {
+	// offset := offsets[idx]
+	for i, idx := range rand.Perm(len(miceMbps)) {
+		mouseMbps := miceMbps[idx]
+
+		offset := offsets[0]
+
+		var startEleph, startMouse time.Time
+		durationEleph := duration
+		var durationMouse time.Duration
+		if offset >= 0 {
+			startEleph = start
+			startMouse = start.Add(offset)
+			// durationMouse = duration + (duration - offset) // End at start + 2 * duration
+			durationMouse = duration
+		} else {
+			startEleph = start.Add(-offset)
+			startMouse = start
+			durationMouse = 2 * duration // End at start + 2 * duration
+		}
+
+		nameEleph := fmt.Sprintf("%v_%d_eleph_%03d.csv", direction.StringLower(), i, elephMbps)
+		nameMouse := fmt.Sprintf("%v_%d_mouse_%03d.csv", direction.StringLower(), i, mouseMbps)
+
+		e.RunClient(&pkg.SenderClient{
+			IP:        e.IP,
+			Out:       filepath.Join(resultPath, nameEleph),
+			Direction: direction,
+			StartAt:   startEleph,
+			Sender: &pkg.RateSender{Params: []pkg.RateParams{pkg.RateParams{
+				Pps:         uint(elephMbps*1e6) / 8 / 1400,
+				Interval:    time.Millisecond,
+				Duration:    durationEleph,
+				PayloadSize: 1400,
+			}}},
+		})
+
+		e.RunClient(&pkg.SenderClient{
+			IP:        e.IP,
+			Out:       filepath.Join(resultPath, nameMouse),
+			Direction: direction,
+			StartAt:   startMouse,
+			Sender: &pkg.RateSender{Params: []pkg.RateParams{pkg.RateParams{
+				Pps:         uint(mouseMbps*1e6) / 8 / 1400,
+				Interval:    time.Millisecond,
+				Duration:    durationMouse,
+				PayloadSize: 1400,
+			}}},
+		})
+
+		start = start.Add(2 * duration).Add(spacing)
+
+		// Check if another test still fits into the current RI
+		if start.Add(2 * duration).Add(spacing).After(deadline) {
+			break outer
+		}
+	}
+	// }
+
+	e.tcpdump(resultPath, ts, start.Sub(ts)+time.Second)
+
+	return nil
+}
+
 func (e *Executor) QUIC(ts time.Time, resultPath string, params ParamMap) error {
 	direction, err := params.Direction()
 	if err != nil {
@@ -587,6 +703,60 @@ func (e *Executor) QUIC(ts time.Time, resultPath string, params ParamMap) error 
 	})
 
 	e.tcpdump(resultPath, ts, start.Sub(ts)+time.Second)
+
+	return nil
+}
+
+func (e *Executor) ProgressiveDurationQUIC(ts time.Time, resultPath string, params ParamMap) error {
+	direction, err := params.Direction()
+	if err != nil {
+		return fmt.Errorf("Procedure requires valid 'direction' param: %v", err.Error())
+	}
+
+	gap := 1000 * time.Millisecond
+	start := ts.Add(1 * time.Second)
+	deadline := nextRi(ts).Add(-time.Second)
+
+	// durationsMs := []int{100, 300, 500, 700, 900, 1400, 2000}
+	durationsMs := []uint{100, 400, 1000, 5000}
+	if _, ok := params["durations"]; ok {
+		var err error
+		if durationsMs, err = params.Uints("durations"); err != nil {
+			slog.Error("parse duration", "error", err)
+			os.Exit(1)
+		}
+	}
+
+	// Execute the tasks in random order
+	for _, idx := range rand.Perm(len(durationsMs)) {
+		durationMs := durationsMs[idx]
+		duration := time.Duration(durationMs) * time.Millisecond
+
+		nextStart := start.Add(duration).Add(gap)
+		if nextStart.After(deadline) {
+			// Would take too much time
+			break
+		}
+
+		e.RunClient(&pkg.SenderClient{
+			IP:        e.IP,
+			Out:       filepath.Join(resultPath, fmt.Sprintf("quic_%v_%04d.csv", direction.StringLower(), durationMs)),
+			Direction: direction,
+			StartAt:   start,
+			Sender: &pkg.QUICSender{Params: pkg.QUICParams{
+				Duration_: duration,
+				Bytes:     1 << 32,
+			}},
+		})
+
+		start = nextStart
+	}
+
+	if start.After(deadline) {
+		panic(fmt.Sprintf("Too many tests: %v > %v", start, deadline))
+	}
+
+	e.tcpdump(resultPath, ts, 15*time.Second)
 
 	return nil
 }
